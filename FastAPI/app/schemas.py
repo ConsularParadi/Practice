c547affdbc7ab3908b150e2284317d6f8dd6425a
Rishi Sharma <consularparadi@gmail.com>
2023-09-27 09:09:18 +0530

    FastAPI Blog Project

---
+++ FastAPI/app/schemas.py
@@ -0,0 +1,35 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+class Blog(BaseModel):
+    title: str
+    body: str
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog]
+    class Config():
+        orm_mode=True
+
+class ShowBlog(BaseModel):
+    title:str
+    author:ShowUser
+    class Config():
+        orm_mode=True
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str]=None
